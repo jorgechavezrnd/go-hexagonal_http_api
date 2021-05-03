1ee7ea4ff055c50140a23d6273a01fd9843f950b
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-05-02 20:03:44 -0400

    Subscriver for Course Created domain event, added

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -7,7 +7,9 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	mooc "github.com/jorgechavezrnd/go-hexagonal_http_api/internal"
 	"github.com/jorgechavezrnd/go-hexagonal_http_api/internal/creating"
+	"github.com/jorgechavezrnd/go-hexagonal_http_api/internal/increasing"
 	"github.com/jorgechavezrnd/go-hexagonal_http_api/internal/platform/bus/inmemory"
 	"github.com/jorgechavezrnd/go-hexagonal_http_api/internal/platform/server"
 	"github.com/jorgechavezrnd/go-hexagonal_http_api/internal/platform/storage/mysql"
@@ -41,9 +43,15 @@ func Run() error {
 	courseRepository := mysql.NewCourseRepository(db, dbTimeout)
 
 	creatingCourseService := creating.NewCourseSerivce(courseRepository, eventBus)
+	increasingCourseCounterService := increasing.NewCourseCounterService()
 
 	createCourseCommandHandler := creating.NewCourseCommandHandler(creatingCourseService)
 	commandBus.Register(creating.CourseCommandType, createCourseCommandHandler)
+
+	eventBus.Subscribe(
+		mooc.CourseCreatedEventType,
+		creating.NewIncreaseCoursesCounterOnCourseCreated(increasingCourseCounterService),
+	)
 
 	ctx, srv := server.New(context.Background(), host, port, shutdownTimeout, commandBus)
 	return srv.Run(ctx)
--- kit/event/event.go
+++ kit/event/event.go
@@ -11,9 +11,16 @@ import (
 type Bus interface {
 	// Publish in the method used to publish new events.
 	Publish(context.Context, []Event) error
+	// Subscribe is the method used to subscribe new event handlers.
+	Subscribe(Type, Handler)
 }
 
 // go:generate mockery --case=snake --outpkg=eventmocks --output=eventmocks --name=Bus
+
+// Handler defines the expected behaviour from an event handler.
+type Handler interface {
+	Handle(context.Context, Event) error
+}
 
 // Type represents a domain event type.
 type Type string
