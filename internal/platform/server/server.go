e29b7c49d0ed72a9e3cd4d89c233c68b6a326f95
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-05-04 23:35:20 -0400

    Some names, refactored

--- internal/platform/server/server.go
+++ internal/platform/server/server.go
@@ -27,12 +27,12 @@ type Server struct {
 	commandBus command.Bus
 }
 
-func New(ctx context.Context, host string, port uint, shudownTimeout time.Duration, commandBus command.Bus) (context.Context, Server) {
+func New(ctx context.Context, host string, port uint, shutdownTimeout time.Duration, commandBus command.Bus) (context.Context, Server) {
 	srv := Server{
 		engine:   gin.New(),
 		httpAddr: fmt.Sprintf("%s:%d", host, port),
 
-		shutdownTimeout: shudownTimeout,
+		shutdownTimeout: shutdownTimeout,
 
 		commandBus: commandBus,
 	}
